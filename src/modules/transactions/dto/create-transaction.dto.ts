d46064d1ed839e4f42447eaaecf9e5d7757f76d7
arkmadj <ahmadjinadu@gmail.com>
2023-04-12 17:49:49 +0100

    added paid by in the create transaction dto

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,27 @@
-export class CreateTransactionDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  settlement_currency: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  fees: number;
+
+  @IsString()
+  @IsNotEmpty()
+  status: string;
+
+  @IsString()
+  @IsNotEmpty()
+  paid_by: string;
+
+  @IsString()
+  @IsNotEmpty()
+  waitlist_comment: string;
+}
