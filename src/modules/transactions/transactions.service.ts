08e809d8ddc371c5b071ee4f530a41301263989d
arkmadj <ahmadjinadu@gmail.com>
2023-04-13 11:18:52 +0100

    fixed transaction fees and sub total calculations

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -30,48 +30,52 @@ export class TransactionsService {
        This is not completed I don't know the way the transaction is 
        on the payload we are sending for aggragators
        **/
-      const createTransactionDto = createTransactionDto_.map(item => {
+      const createTransactionDto = createTransactionDto_.map((item) => {
         const randomString = randomstring.generate({
           length: 9,
           charset: 'numeric',
         });
         const today = new Date();
         const nextWeek = new Date();
-        nextWeek.setDate(nextWeek.getDate() + 7)
+        nextWeek.setDate(nextWeek.getDate() + 7);
         return {
           ...item,
           pin: `NGN${randomString} (TRG)`,
-          date: (today).toISOString().split('T')[0], // convert to YYYY-MM-DD
-          completed_date: (nextWeek).toISOString().split('T')[0], // convert to YYYY-MM-DD in a future date
-          amount_destination: item.amount * Number(process.env.CONVERSION_RATE)
-        }
-      })
-      const transactions = createTransactionDto.map((transaction:CreateTransactionType) => ({
-        client: {
-          business_name: transaction.paid_by,
-          contact_number: ''
-        },
-        transaction_reference: transaction.pin,
-        transaction_date: transaction.date,
-        expected_settlement_date: transaction.completed_date,
-        category: 'Money Exchange',
-        amount: transaction.amount_destination,
-        number_of_transactions: 1,
-        transaction_fees: transaction.amount * Number(process.env.MARGIN),
-        status: "success"
-      }));
+          date: today.toISOString().split('T')[0], // convert to YYYY-MM-DD
+          completed_date: nextWeek.toISOString().split('T')[0], // convert to YYYY-MM-DD in a future date
+          amount_destination: item.amount * Number(process.env.CONVERSION_RATE),
+        };
+      });
+      const transactions = createTransactionDto.map(
+        (transaction: CreateTransactionType) => ({
+          client: {
+            business_name: transaction.paid_by,
+            contact_number: '',
+          },
+          transaction_reference: transaction.pin,
+          transaction_date: transaction.date,
+          expected_settlement_date: transaction.completed_date,
+          category: 'Money Exchange',
+          amount: transaction.amount_destination,
+          number_of_transactions: 1,
+          transaction_fees: transaction.amount * Number(process.env.MARGIN),
+          status: 'success',
+        }),
+      );
 
-      const financed_transactions = createTransactionDto.map((transaction:CreateTransactionType) => ({
-        transaction_reference: transaction.pin,
-        amount: transaction.amount_destination,
-        payments: [
-          {
-            disbursement_date: transaction.date,
-            expected_payment_date: transaction.completed_date,
-            amount: transaction.amount_destination
-          }
-        ]
-      }));
+      const financed_transactions = createTransactionDto.map(
+        (transaction: CreateTransactionType) => ({
+          transaction_reference: transaction.pin,
+          amount: transaction.amount_destination,
+          payments: [
+            {
+              disbursement_date: transaction.date,
+              expected_payment_date: transaction.completed_date,
+              amount: transaction.amount_destination,
+            },
+          ],
+        }),
+      );
 
       const response = await this.axiosInstance.post(`/transactions/register`, {
         customer_partner_id: this.customerPartnerId,
@@ -89,9 +93,10 @@ export class TransactionsService {
 
       // This is not completed
       for (const transaction of createTransactionDto) {
-        const amount =
-          Number(transaction.amount +
-          (transaction.amount * Number(process.env.MARGIN)).toFixed(2));
+        const amount = Number(
+          transaction.amount +
+            (transaction.amount * Number(process.env.MARGIN)).toFixed(2),
+        );
 
         const body = {
           pin: transaction.pin,
@@ -100,7 +105,7 @@ export class TransactionsService {
           amount,
           amount_revised: amount,
           fees: transaction.fees,
-          sub_total: amount + transaction.amount * Number(process.env.MARGIN),
+          sub_total: amount + transaction.fees,
           amount_destination: transaction.amount_destination,
           transaction_status: transaction.status,
           completed_date: transaction.completed_date,
@@ -110,7 +115,7 @@ export class TransactionsService {
 
         const transactionData = this.transactionRepository.create(body);
         await this.transactionRepository.save(transactionData);
-      };
+      }
     } catch (error) {
       return {
         status: error,
